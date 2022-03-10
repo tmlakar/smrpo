0733a7df8f75152ea41535387a71044d49e43b37
Tjaša Mlakar <tjasamlakar0@gmail.com>
2022-03-10 15:29:20 +0100

    models admin.js, user.js

---
+++ app_server/models/user.js
@@ -0,0 +1,9 @@
+[{
+    "name": "Tjaša",
+    "surname": "Mlakar",
+    "username": "tmlakar1",
+    "email": "tjasamlakar111@gmail.com",
+    "password": "scrumly001",
+    "role": "administrator"
+}
+]
