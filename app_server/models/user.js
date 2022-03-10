46f75c54d5371af0642be8ff47eb0cf9b1dfcfe0
Tjaša Mlakar <tjasamlakar0@gmail.com>
2022-03-10 16:08:24 +0100

    models

--- app_server/models/user.js
+++ app_server/models/user.js
@@ -5,5 +5,29 @@
     "email": "tjasamlakar111@gmail.com",
     "password": "scrumly001",
     "role": "administrator"
+},
+{
+    "name": "Anja",
+    "surname": "Brelih",
+    "username": "abrelih1",
+    "email": "ab@gmail.com",
+    "password": "scrumly002",
+    "role": "administrator"
+},
+{
+    "name": "Eva",
+    "surname": "Vidmar",
+    "username": "evavid1",
+    "email": "ev@gmail.com",
+    "password": "scrumly001",
+    "role": "user"
+},
+{
+    "name": "Ana",
+    "surname": "Knafelc",
+    "username": "anakna",
+    "email": "ak@gmail.com",
+    "password": "scrumly001",
+    "role": "user"
 }
 ]
