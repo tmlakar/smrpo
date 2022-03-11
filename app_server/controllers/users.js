6df793f64a56f1a5c3a86d42ee038a0c9cd59d47
Tjaša Mlakar <tjasamlakar0@gmail.com>
2022-03-11 14:48:19 +0100

    Povezovanje REST API in streznika za prikazovanje podatkov iz baze

--- app_server/controllers/users.js
+++ app_server/controllers/users.js
@@ -1,11 +1,27 @@
+
+var apiParametri = {
+    streznik: "http://localhost:" + (process.env.PORT || 3000),
+  };
+  if (process.env.NODE_ENV === "production") {
+    
+  }
+  const axios = require("axios").create({
+    baseURL: apiParametri.streznik,
+    timeout: 5000,
+  });
+
+
 var users = require("../models/user.json");
 
+
 var seznam = (req, res) => {
-
-    res.render('users-admin', {
-        users: users
-
-    });
+    
+  axios
+      .get (apiParametri.streznik + '/api/users', {})
+      .then((odgovor) => {
+          res.render('users-admin', {
+          users: odgovor.data});
+      });
 };
 
 
