e178074c2358453df9cce219e7e1f96550297019
Tjaša Mlakar <tjasamlakar0@gmail.com>
2022-04-03 21:41:51 +0200

    axios.all

--- app_server/controllers/collaborators.js
+++ app_server/controllers/collaborators.js
@@ -11,22 +11,50 @@ var apiParametri = {
 
   /* seznam userjev, ki se jih lahko doda na projekt */
   var availableCollaboratorsList = (req, res) => {
-
+    var projectId = req.params.id;
     var tokenParts = req.cookies.authcookie['žeton'].split('.');
     var encodedPayload = tokenParts[1];
     //var rawPayload = window.atob(encodedPayload);
     var rawPayload = Buffer.from(encodedPayload, 'base64').toString('ascii');
     var user = JSON.parse(rawPayload);
+
+    // axios.all([ 
+    //   axios.get (apiParametri.streznik + '/api/users'),
+    //   axios.get (apiParametri.streznik + '/api/projects/' + projectId)
+    //   ])
+    //   .catch((error) => {
+    //     console.log(error);
+    //   })
+    //   .then(axios.spread((object1, object2) => {
+    //       console.log(object1, object2);
+    // }))
   
     axios
         .get (apiParametri.streznik + '/api/users', {})
         .then((odgovor) => {
-            res.send(odgovor.data);
+          res.render('project-edit', {
+            users: odgovor.data
+          });
           })
+
+          
+
+    
   };
 
   /* Podrobnosti projekta */
   var podrobnostiProject = (req, res) => {
+    var tokenParts = req.cookies.authcookie['žeton'].split('.');
+    var encodedPayload = tokenParts[1];
+    //var rawPayload = window.atob(encodedPayload);
+    var rawPayload = Buffer.from(encodedPayload, 'base64').toString('ascii');
+    var user = JSON.parse(rawPayload);
+      var projectId = req.params.id;
+      var napaka = req.query.error;
+      var jeNapaka = false;
+      if(napaka == "napaka"){
+        jeNapaka = true;
+      }
       axios
           .get (apiParametri.streznik + '/api/projects/' + projectId)
           .then((odgovor) => {
@@ -35,10 +63,12 @@ var apiParametri = {
                 info: odgovor.data.info,
                 collaborators: odgovor.data.collaborators,
                 id: odgovor.data._id,
-                
+                napaka: jeNapaka
               });
           });
-    };
+    
+
+  };
 
 /* POST - Add project collaborators */
 const addProjectCollaborators = (req, res) => {
