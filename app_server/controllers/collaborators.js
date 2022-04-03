06173443e8d71d6cb5abe47d123ff47aee948a7f
Tjaša Mlakar <tjasamlakar0@gmail.com>
2022-04-03 12:34:12 +0200

    collaborators streznik

--- app_server/controllers/collaborators.js
+++ app_server/controllers/collaborators.js
@@ -9,7 +9,7 @@ var apiParametri = {
     timeout: 5000,
   });
 
-  /* seznam userjev */
+  /* seznam userjev, ki se jih lahko doda na projekt */
   var availableCollaboratorsList = (req, res) => {
 
     var tokenParts = req.cookies.authcookie['žeton'].split('.');
@@ -21,118 +21,39 @@ var apiParametri = {
     axios
         .get (apiParametri.streznik + '/api/users', {})
         .then((odgovor) => {
-            prikaziStran(req, res, odgovor.data)
+            res.send(odgovor.data);
           })
   };
 
-
-/* Add project collaborators */
-
-/* GET metoda (prikaz) - Dodajanje uporabnikov na projekt in določitev projektnih vlog
-  s strani admina na /projects/unikaten_id_projekta/add-collaborators */
-  // prikazovanje vseh userjev z role user iz sistema, med katerimi lahko admin izbere
-//   const addCollaboratorsDisplay = (req, res) => {
-//     var tokenParts = req.cookies.authcookie['žeton'].split('.');
-//     var encodedPayload = tokenParts[1];
-//     //var rawPayload = window.atob(encodedPayload);
-//     var rawPayload = Buffer.from(encodedPayload, 'base64').toString('ascii');
-//     var user = JSON.parse(rawPayload);
-//       var projectId = req.params.id;
-//       var napaka = req.query.error;
-//       var jeNapaka = false;
-//       if(napaka == "napaka"){
-//         jeNapaka = true;
-//       }
-  
-//       var users;
-  
-  
-//       axios
-//           .get (apiParametri.streznik + '/api/projects/' + projectId)
-//           .then((odgovor) => {
-//               res.render('project-add-collaborators',
-//               { name: odgovor.data.name,
-//                 info: odgovor.data.info,
-//                 collaborators: odgovor.data.users,
-//                 id: odgovor.data._id,
-//                 napaka: jeNapaka,
-//                 users: users
-//               });
-//           });
-  
-//   }
-  
-//   /* PUT metoda - Dodajanje uporabnikov na projekt in določitev projektnih vlog
-//   s strani admina na /projects/unikaten_id_projekta/add-collaborators */
-  
-//   /* Edit project collaborators */
-  
-//   /* GET metoda (prikaz) - Urejanje uporabnikov oziroma njihovih projektnih vlog
-//     s strani admina na /projects/unikaten_id_projekta/edit-collaborator-roles */
-//     const editCollaboratorsDisplay = (req, res) => {
-//       var tokenParts = req.cookies.authcookie['žeton'].split('.');
-//       var encodedPayload = tokenParts[1];
-//       //var rawPayload = window.atob(encodedPayload);
-//       var rawPayload = Buffer.from(encodedPayload, 'base64').toString('ascii');
-//       var user = JSON.parse(rawPayload);
-//         var projectId = req.params.id;
-//         var napaka = req.query.error;
-//         var jeNapaka = false;
-//         if(napaka == "napaka"){
-//           jeNapaka = true;
-//         }
-//         axios
-//             .get (apiParametri.streznik + '/api/projects/' + projectId)
-//             .then((odgovor) => {
-//                 res.render('project-edit-collaborator-roles',
-//                 { name: odgovor.data.name,
-//                   info: odgovor.data.info,
-//                   collaborators: odgovor.data.users,
-//                   id: odgovor.data._id,
-//                   napaka: jeNapaka
-//                 });
-//             });
-  
-//     }
-  
-//     /* PUT metoda  - Urejanje uporabnikov oziroma njihovih projektnih vlog
-//     s strani admina na /projects/unikaten_id_projekta/edit-collaborator-roles */
-  
-//   /* Delete project collaborators */
-  
-//   /* GET metoda (prikaz) - Brisanje uporabnikov s projekta
-//     s strani admina na /projects/unikaten_id_projekta/delete-collaborators */
-//     const deleteCollaboratorsDisplay = (req, res) => {
-//       var tokenParts = req.cookies.authcookie['žeton'].split('.');
-//       var encodedPayload = tokenParts[1];
-//       //var rawPayload = window.atob(encodedPayload);
-//       var rawPayload = Buffer.from(encodedPayload, 'base64').toString('ascii');
-//       var user = JSON.parse(rawPayload);
-//         var projectId = req.params.id;
-//         var napaka = req.query.error;
-//         var jeNapaka = false;
-//         if(napaka == "napaka"){
-//           jeNapaka = true;
-//         }
-//         axios
-//             .get (apiParametri.streznik + '/api/projects/' + projectId)
-//             .then((odgovor) => {
-//                 res.render('project-delete-collaborators',
-//                 { name: odgovor.data.name,
-//                   info: odgovor.data.info,
-//                   collaborators: odgovor.data.users,
-//                   id: odgovor.data._id,
-//                   napaka: jeNapaka
-//                 });
-//             });
-  
-//     }
-  
-    /* PUT metoda  - Brisanje uporabnikov s projekta
-    s strani admina na /projects/unikaten_id_projekta/delete-collaborators */
-  
-    module.exports = {
-      availableCollaboratorsList
-        
+  /* Podrobnosti projekta */
+  var podrobnostiProject = (req, res) => {
+      axios
+          .get (apiParametri.streznik + '/api/projects/' + projectId)
+          .then((odgovor) => {
+              res.render('project-edit',
+              { name: odgovor.data.name,
+                info: odgovor.data.info,
+                collaborators: odgovor.data.collaborators,
+                id: odgovor.data._id,
+                
+              });
+          });
     };
+
+/* POST - Add project collaborators */
+const addProjectCollaborators = (req, res) => {
+  const idProject = req.params.idProject;
+  
+};
+
+/* PUT - Edit collaborators roles */
+
+/* DELETE - Remove collaborators from a projects */
+
+module.exports = {
+  availableCollaboratorsList,
+  podrobnostiProject,
+  addProjectCollaborators
+        
+};
     
