8c8742029219a0e2e2d14f7d8e74b60cc10cf07f
Tjaša Mlakar <tjasamlakar0@gmail.com>
2022-04-06 10:17:47 +0200

    nastavitev orodne vrstice pri project backlogu za userja

--- app_server/controllers/project.js
+++ app_server/controllers/project.js
@@ -12,14 +12,31 @@ var apiParametri = {
 
 
   var prikaz = (req, res) => {
+    var tokenParts = req.cookies.authcookie['žeton'].split('.');
+    var encodedPayload = tokenParts[1];
+    var rawPayload = Buffer.from(encodedPayload, 'base64').toString('ascii');
+    var user = JSON.parse(rawPayload);
+    var vloga = user.role;
+
     var projectId = req.params.id;
     axios
         .get (apiParametri.streznik + '/api/projects/' + projectId)
         .then((odgovor) => {
+          if (vloga == "user") {
             res.render('project',
             { name: odgovor.data.name,
               id: projectId,
-              sprints: odgovor.data.sprints 
+              sprints: odgovor.data.sprints,
+              layout: 'layout-user'
+            });
+          } else {
+
+          }
+            res.render('project',
+            { name: odgovor.data.name,
+              id: projectId,
+              sprints: odgovor.data.sprints,
+              layout: 'layout'
             });
         });
   };
