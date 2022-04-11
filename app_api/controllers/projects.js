a81db0d306bc648f9eb339cdcea1151c03c0e869
anjabrelih <ab0555@student.uni-lj.si>
2022-04-12 00:00:11 +0200

    user feedback story name duplicate

--- app_api/controllers/projects.js
+++ app_api/controllers/projects.js
@@ -3,10 +3,10 @@ const Project = mongoose.model("Project");
 
 /* Projects list */
 const projectsList = (req, res) => {
-    Project.find().exec(function (err, projects) {
+    Project.find().exec(function(err, projects) {
         if (err) {
             console.log(err);
-            res.status(404).json({"sporočilo": "Napaka pri poizvedbi: " + err});
+            res.status(404).json({ "sporočilo": "Napaka pri poizvedbi: " + err });
         } else {
             res.status(200).json(projects);
         }
@@ -17,38 +17,38 @@ const projectsList = (req, res) => {
 /* Creating new project */
 
 const projectCreate = (req, res) => {
-  //var collaboratorsR = [{username: "anja", project_role: "Team Member"}];
-  console.log(req.body.name, req.body.info);
-  
+    //var collaboratorsR = [{username: "anja", project_role: "Team Member"}];
+    console.log(req.body.name, req.body.info);
+
     Project.create({
 
-      name: req.body.name,
-      info: req.body.info
+        name: req.body.name,
+        info: req.body.info
 
-  }, (napaka, project) => {
-      if(napaka) {
-        console.log("Prislo je do napake");
-          res.status(400).json(napaka);
-      }  else {
-          res.status(201).json(project);
-      }
+    }, (napaka, project) => {
+        if (napaka) {
+            console.log("Prislo je do napake");
+            res.status(400).json(napaka);
+        } else {
+            res.status(201).json(project);
+        }
     });
 };
 
 /* Just for developing purposes - deleting a project */
 const deleteProject = (req, res) => {
-  const { idProject } = req.params;
+    const { idProject } = req.params;
     if (idProject) {
-      Project.findByIdAndRemove(idProject).exec((napaka) => {
-        if (napaka) {
-          return res.status(500).json(napaka);
-        }
-        res.status(204).json("uspešno");
-      });
+        Project.findByIdAndRemove(idProject).exec((napaka) => {
+            if (napaka) {
+                return res.status(500).json(napaka);
+            }
+            res.status(204).json("uspešno");
+        });
     } else {
-      res.status(404).json({
-        sporočilo: "Ne najdem projekta, idProject je obvezen parameter.",
-      });
+        res.status(404).json({
+            sporočilo: "Ne najdem projekta, idProject je obvezen parameter.",
+        });
     }
 };
 
@@ -57,47 +57,46 @@ const deleteProject = (req, res) => {
 /* Particular project info */
 
 const projectInfo = (req, res) => {
-    Project.findById(req.params.idProject).exec((napaka,project) => {
-      if (!project) {
-        return res
-          .status(404)
-          .json({
-            sporočilo:
-              "Ne najdem projekta s podanim enoličnim identifikatorjem idProject.",
-          });
-      } else if (napaka) {
-        return res.status(500).json(napaka);
-      }
-      res.status(200).json(project);
+    Project.findById(req.params.idProject).exec((napaka, project) => {
+        if (!project) {
+            return res
+                .status(404)
+                .json({
+                    sporočilo: "Ne najdem projekta s podanim enoličnim identifikatorjem idProject.",
+                });
+        } else if (napaka) {
+            return res.status(500).json(napaka);
+        }
+        res.status(200).json(project);
     });
 };
 
 /* Updating particular project */
 const projectUpdate = (req, res) => {
     if (!req.params.idProject) {
-      return res.status(404).json({
-        sporočilo: "Ne najdem projekta, idProject je obvezen parameter.",
-      });
+        return res.status(404).json({
+            sporočilo: "Ne najdem projekta, idProject je obvezen parameter.",
+        });
     }
     Project.findById(req.params.idProject)
-      .exec((napaka, project) => {
-        if (!project) {
-          return res.status(404).json({ sporočilo: "Ne najdem projekta." });
-        } else if (napaka) {
-          return res.status(500).json(napaka);
-        }
-        project.name = req.body.name;
-        project.info = req.body.info;
+        .exec((napaka, project) => {
+            if (!project) {
+                return res.status(404).json({ sporočilo: "Ne najdem projekta." });
+            } else if (napaka) {
+                return res.status(500).json(napaka);
+            }
+            project.name = req.body.name;
+            project.info = req.body.info;
 
-        project.save((napaka, project) => {
-          if (napaka) {
-            res.status(404).json(napaka);
-          } else {
-            res.status(200).json(project);
-          }
+            project.save((napaka, project) => {
+                if (napaka) {
+                    res.status(404).json(napaka);
+                } else {
+                    res.status(200).json(project);
+                }
+            });
         });
-      });
-  };
+};
 
 module.exports = {
     projectsList,
--- app_server/controllers/collaborators.js
+++ app_server/controllers/collaborators.js
@@ -1,16 +1,16 @@
 var apiParametri = {
     streznik: "http://localhost:" + (process.env.PORT || 3000),
-  };
-  if (process.env.NODE_ENV === "production") {
+};
+if (process.env.NODE_ENV === "production") {
 
-  }
-  const axios = require("axios").create({
+}
+const axios = require("axios").create({
     baseURL: apiParametri.streznik,
     timeout: 5000,
-  });
+});
 
-  /* seznam userjev, ki se jih lahko doda na projekt */
-  var availableCollaboratorsList = (req, res) => {
+/* seznam userjev, ki se jih lahko doda na projekt */
+var availableCollaboratorsList = (req, res) => {
     var projectId = req.params.id;
     var tokenParts = req.cookies.authcookie['žeton'].split('.');
     var encodedPayload = tokenParts[1];
@@ -21,173 +21,179 @@ var apiParametri = {
     var vloga = user.role;
     var layout1 = 'layout';
     if (vloga == "user") {
-      layout1 = "layout-user";
+        layout1 = "layout-user";
     }
 
     // je napaka?
     var napaka = req.query.error;
     var jeNapaka = false;
-    if(napaka == "napakaPriDodajanjuUserja"){
-      jeNapaka = true;
+    if (napaka == "napakaPriDodajanjuUserja") {
+        jeNapaka = true;
     }
-   // je succesfully removed
-   var success = req.query.error;
-   var uspesnoRemoved = false;
-   if(success == "success"){
-    uspesnoRemoved = true;
-  }
-  // je successfully added
-  var success = req.query.error;
-  var uspesnoAdded = false;
-  if(success == "successfully added"){
-   uspesnoAdded = true;
- }
- // je successfully edited
- var successfullyEdited = req.query.edit;
- var uspesnoEdited = false;
-  if(successfullyEdited == "successfully edited"){
-   uspesnoEdited = true;
- }
+    // je succesfully removed
+    var success = req.query.error;
+    var uspesnoRemoved = false;
+    if (success == "success") {
+        uspesnoRemoved = true;
+    }
+    // je successfully added
+    var success = req.query.error;
+    var uspesnoAdded = false;
+    if (success == "successfully added") {
+        uspesnoAdded = true;
+    }
+    // je successfully edited
+    var successfullyEdited = req.query.edit;
+    var uspesnoEdited = false;
+    if (successfullyEdited == "successfully edited") {
+        uspesnoEdited = true;
+    }
+    // duplikat pri spreminjanju imena projekta
+    var napakaEdit = false;
+    if (req.query.error == "napaka") {
+        napakaEdit = true;
+    }
 
- var successfullyEditedP = req.query.editp;
-  var uspesnoPosodobljeno = false;
-  if (successfullyEditedP == "success") {
-    uspesnoPosodobljeno = true;
-  }
+    var successfullyEditedP = req.query.editp;
+    var uspesnoPosodobljeno = false;
+    if (successfullyEditedP == "success") {
+        uspesnoPosodobljeno = true;
+    }
 
-          let URL1 = apiParametri.streznik + '/api/users';
-          let URL2 = apiParametri.streznik + '/api/projects/' + projectId;
-        
-          const promise1 = axios.get(URL1);
-          const promise2 = axios.get(URL2);
-          
+    let URL1 = apiParametri.streznik + '/api/users';
+    let URL2 = apiParametri.streznik + '/api/projects/' + projectId;
 
-          Promise.all([promise1, promise2]).then(function(values) {
-            //console.log(values[0]);
-            console.log("working?");
-            //console.log(values[1]);
-            res.render("project-edit", {
-              name: values[1].data.name,
-              info: values[1].data.info,
-              collaborators: values[1].data.collaborators,
-              id: values[1].data._id,
-              users: values[0].data,
-              napaka1: jeNapaka,
-              success: uspesnoRemoved,
-              successfullyAdded: uspesnoAdded,
-              successfullyEdited: uspesnoEdited,
-              successfullyEditedProject: uspesnoPosodobljeno,
-              layout: layout1
-            });
-          });
-  };
+    const promise1 = axios.get(URL1);
+    const promise2 = axios.get(URL2);
 
-  
-  /* Podrobnosti projekta */
-  var podrobnostiProject = (req, res) => {
+
+    Promise.all([promise1, promise2]).then(function(values) {
+        //console.log(values[0]);
+        console.log("working?");
+        //console.log(values[1]);
+        res.render("project-edit", {
+            name: values[1].data.name,
+            info: values[1].data.info,
+            collaborators: values[1].data.collaborators,
+            id: values[1].data._id,
+            users: values[0].data,
+            napaka1: jeNapaka,
+            success: uspesnoRemoved,
+            successfullyAdded: uspesnoAdded,
+            successfullyEdited: uspesnoEdited,
+            successfullyEditedProject: uspesnoPosodobljeno,
+            layout: layout1,
+            napaka: napakaEdit,
+        });
+    });
+};
+
+
+/* Podrobnosti projekta */
+var podrobnostiProject = (req, res) => {
     var tokenParts = req.cookies.authcookie['žeton'].split('.');
     var encodedPayload = tokenParts[1];
     //var rawPayload = window.atob(encodedPayload);
     var rawPayload = Buffer.from(encodedPayload, 'base64').toString('ascii');
     var user = JSON.parse(rawPayload);
-      var projectId = req.params.id;
-      var napaka = req.query.error;
-      var jeNapaka = false;
-      
-      if(napaka == "napaka"){
-        jeNapaka = true;
-      }
+    var projectId = req.params.id;
+    var napaka = req.query.error;
+    var jeNapaka = false;
 
-      console.log(jeNapaka);
-      axios
-          .get (apiParametri.streznik + '/api/projects/' + projectId)
-          .then((odgovor) => {
-              res.render('project-edit',
-              { name: odgovor.data.name,
+    if (napaka == "napaka") {
+        jeNapaka = true;
+    }
+
+    console.log(jeNapaka);
+    axios
+        .get(apiParametri.streznik + '/api/projects/' + projectId)
+        .then((odgovor) => {
+            res.render('project-edit', {
+                name: odgovor.data.name,
                 info: odgovor.data.info,
                 collaborators: odgovor.data.collaborators,
                 id: odgovor.data._id,
                 napaka: jeNapaka
-              });
-          });
-    
+            });
+        });
 
-  };
+
+};
 
 /* POST - Add project collaborators */
 const addProjectCollaborators = (req, res) => {
-  var projectId = req.params.id;
-  if (!req.body.username || !req.body.project_role) {
+    var projectId = req.params.id;
+    if (!req.body.username || !req.body.project_role) {
 
-  } else {
-    axios({
-      method: 'post',
-      url: apiParametri.streznik + '/api/projects/' + projectId + '/add-collaborators',
-      data: {
-        username: req.body.username,
-        project_role: req.body.project_role
-      }
-    }).then(() => {
-      var string = "successfully added";
-      res.redirect('/projects/' + projectId + '?error=' + string);
-    }).catch((napaka) => {
-      var string = "napakaPriDodajanjuUserja";
-    res.redirect('/projects/' + projectId + '?error=' + string);
+    } else {
+        axios({
+            method: 'post',
+            url: apiParametri.streznik + '/api/projects/' + projectId + '/add-collaborators',
+            data: {
+                username: req.body.username,
+                project_role: req.body.project_role
+            }
+        }).then(() => {
+            var string = "successfully added";
+            res.redirect('/projects/' + projectId + '?error=' + string);
+        }).catch((napaka) => {
+            var string = "napakaPriDodajanjuUserja";
+            res.redirect('/projects/' + projectId + '?error=' + string);
 
-    });
-  }
+        });
+    }
 };
 
 /* PUT - Edit collaborators roles */
 
 const editProjectCollaboratorRole = (req, res) => {
-  var projectId = req.params.id;
-  var collaboratorId = req.params.idC;
-  if (!req.body.project_role) {
+    var projectId = req.params.id;
+    var collaboratorId = req.params.idC;
+    if (!req.body.project_role) {
 
-  } else {
-    axios({
-      method: 'put',
-      url: apiParametri.streznik + '/api/projects/' + projectId + '/edit-collaborator/' + collaboratorId,
-      data: {
-        project_role: req.body.project_role
-      }
-    }).then(() => {
-      var string = "successfully edited"
-      res.redirect('/projects/' + projectId + '?edit=' + string);
-    }).catch((napaka) => {
-      var string = "napaka";
-    res.redirect('/projects/' + projectId + '?error=' + string);
+    } else {
+        axios({
+            method: 'put',
+            url: apiParametri.streznik + '/api/projects/' + projectId + '/edit-collaborator/' + collaboratorId,
+            data: {
+                project_role: req.body.project_role
+            }
+        }).then(() => {
+            var string = "successfully edited"
+            res.redirect('/projects/' + projectId + '?edit=' + string);
+        }).catch((napaka) => {
+            var string = "napaka";
+            res.redirect('/projects/' + projectId + '?error=' + string);
 
-    });
-  }
+        });
+    }
 };
 
 /* DELETE - Remove collaborators from a projects */
 
 const deleteProjectCollaborator = (req, res) => {
-  var projectId = req.params.id;
-  var collaboratorId = req.params.idC;
-  axios({
-      method: 'delete',
-      url: apiParametri.streznik + '/api/projects/' + projectId + '/delete-collaborator/' + collaboratorId,
-      
+    var projectId = req.params.id;
+    var collaboratorId = req.params.idC;
+    axios({
+        method: 'delete',
+        url: apiParametri.streznik + '/api/projects/' + projectId + '/delete-collaborator/' + collaboratorId,
+
     }).then(() => {
-      var string = "success";
-      res.redirect('/projects/' + projectId + '?error=' + string);
+        var string = "success";
+        res.redirect('/projects/' + projectId + '?error=' + string);
     }).catch((napaka) => {
-      var string = "napaka";
-    res.redirect('/projects/' + projectId + '?error=' + string);
+        var string = "napaka";
+        res.redirect('/projects/' + projectId + '?error=' + string);
 
     });
-  
+
 };
 
 module.exports = {
-  availableCollaboratorsList,
-  podrobnostiProject,
-  addProjectCollaborators,
-  editProjectCollaboratorRole,
-  deleteProjectCollaborator
-        
+    availableCollaboratorsList,
+    podrobnostiProject,
+    addProjectCollaborators,
+    editProjectCollaboratorRole,
+    deleteProjectCollaborator
+
 };
