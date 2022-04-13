97d33e8ba9b9d3c153306315082761689e88781d
Eva Vidmar <ev5864@student.uni-lj.si>
2022-04-14 00:36:08 +0200

    prikaz taskov dodeljenih prijavljenemu

--- app_server/controllers/mytasks.js
+++ app_server/controllers/mytasks.js
@@ -16,13 +16,53 @@ var prikaz = (req, res) => {
     //var rawPayload = window.atob(encodedPayload);
     var rawPayload = Buffer.from(encodedPayload, 'base64').toString('ascii');
     var user = JSON.parse(rawPayload);
-    var projectId = req.params.id;
+    var username = user.username;
 
-    var vloga = user.role;
-    res.render('mytasks', {
-      layout: 'layout-user'
-    });
-  };
+
+    //tukaj moram iz apija pridobit array taskov, ki so mi assginani in ki so v karticah, ki so v sprintih, ki so tekoči
+    //najprej pridobim array projektov
+    axios
+        .get(apiParametri.streznik + '/api/projects')
+        .then((odgovor) => {
+          var myTasks = [];
+          var stolpecTask = 0;
+          var stolpecZgodba = 1;
+          var vrstica = 0;
+          var projekti = odgovor.data;
+          var zgodbe = [];
+          var tasks = [];
+          var myStories = [];
+          var prvic = true;
+          for(var i= 0; i < projekti.length; i++){
+            zgodbe = projekti[i].userStories
+            for(var j=0; j < zgodbe.length; j++){
+              if(zgodbe[j].flags[0] != "Unassigned" && zgodbe[j].flags[1] == "Unfinished"){
+                tasks = zgodbe[j].subtasks;
+                for(var k=0; k<tasks.length; k++){
+                    //nalogo se lahko sploh prikaže, če je prijavljen uporabnik subtask owner, če je pending in če je v aktivnem sprintu
+                    if(tasks[k].subtaskOwnerUsername == username && tasks[k].pending == "true"){
+                      if(prvic){
+                        myTasks = [[tasks[k], zgodbe[j].name]];
+                        prvic = false;
+                    }
+                    else{
+                      myTasks.push([tasks[k],zgodbe[j].name]);
+                    }
+                    }
+                }
+            }
+          }
+          }
+          res.render('mytasks', {
+            layout: 'layout-user',
+            myTasks: myTasks
+
+          });
+          console.log(myTasks)
+        })
+
+
+};
 
 
 module.exports = {
